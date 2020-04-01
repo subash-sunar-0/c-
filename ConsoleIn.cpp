e72e1ab0b40ff0ef267a9b2e177812ae704afe0f
subash <55134019+subash-sunar-0@users.noreply.github.com>
2020-04-01 21:47:33 +0545

    Add files via upload

---
+++ ConsoleIn.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    
+    int i;
+    std::cout<<"Please enter a number: " << std::endl;
+    std::cin>>i;
+    std::cout<<"The entered number is: " << i <<std::endl;
+
+    return 0;
+}
---
+++ String.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    std::string s("This is a string");
+    std::cout<<s.size() <<std::endl;
+    return 0;
+}
